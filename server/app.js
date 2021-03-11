b53d82a6834dd8cd1ec3f325c4fa2dd10bad306e
Tom <tomrambo95@yahoo.com>
2021-03-11 15:03:35 -0500

    added file exports

--- server/app.js
+++ server/app.js
@@ -1,6 +1,13 @@
 const Express = require('express');
 const app = Express();
 
+
+const controllers = require('./controllers');
+
+app.use('/user', controllers.usercontroller);
+app.use('/review', controllers.reviewcontroller);
+
+
 app.listen(3001, () => {
     console.log(`[Server]: App is listening on 3001.`);
 });
