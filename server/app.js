3a05b9c08821f466c1106903c735346accb1d227
Derek <derek101225@gmail.com>
2021-03-11 15:02:03 -0500

    updated the app

--- server/app.js
+++ server/app.js
@@ -1,11 +1,29 @@
-const Express = require('express');
-const app = Express();
+require('dotenv').config();
 
-app.listen(3001, () => {
-    console.log(`[Server]: App is listening on 3001.`);
-});
+const express = require('express');
+const dbConnection = require('./db')
+const middleware = require('./middleware')
+const app = express();
 
 
-//first push and merge to develop
-//develop
+app.use(middleware.CORS);
+app.use(express.json());
+
+const controllers = require('./controllers');
+
+app.use('/review', controllers.reviewcontroller)
+app.use('/user', controllers.usercontroller)
+
+dbConnection.authenticate()
+    .then(() => dbConnection.sync())
+    .then(() => {
+        app.listen(3001, () => {
+            console.log(`[Server]: App is listening on 3001.`);
+        }); 
+    })
+    .catch((err) => {
+        console.log('[Server]: Server Crashed');
+        console.log(err);
+    })
+
 
