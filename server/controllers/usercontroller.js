e5f63d06807f4d58ce08b2fb7ab4f344405a793e
Thomas Blount <Thomasblount89@gmail.com>
2021-03-11 17:48:50 -0500

    thomas made user controllers 1

--- server/controllers/usercontroller.js
+++ server/controllers/usercontroller.js
@@ -1,5 +1,26 @@
 const router = require('express').Router();
 
+//register 
+router.post(" /register", async (req, res) => {
+    const { firstName,
+        lastName,
+        email,
+        password
+    } = req.body;
 
+    try {
+        const newUser = await UserModel.create(
+            {firstName, 
+                lastName,
+                email,
+                password: bcrypt.hashSync(password, 10),
+             });
+    }catch {}
+
+    res.status (201).json ({
+        message: "User registered!"
+
+    })
+ })
 
 module.exports = router;
