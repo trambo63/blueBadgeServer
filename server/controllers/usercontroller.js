3555fc1cc0b64d8d9ddce62bd48518c417271615
Thomas Blount <Thomasblount89@gmail.com>
2021-03-12 11:35:45 -0500

    thomas built out register and login end point- #3 testa

--- server/controllers/usercontroller.js
+++ server/controllers/usercontroller.js
@@ -1,3 +1,5 @@
+const { UniqueConstraintError } = require('sequelize/types');
+
 const router = require('express').Router();
 
 //register 
@@ -10,17 +12,80 @@ router.post(" /register", async (req, res) => {
 
     try {
         const newUser = await UserModel.create(
-            {firstName, 
+            {
+                firstName,
                 lastName,
                 email,
                 password: bcrypt.hashSync(password, 10),
-             });
-    }catch {}
+            });
 
-    res.status (201).json ({
-        message: "User registered!"
+        const token = jwt.sign({
+            id: newUser.id,
+        },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: 60 * 60 * 24,
+            }
+        );
 
-    })
- })
+        res.status(201).json({
+            message: "User registered!",
+            user: newUser,
+            token,
+
+        });
+    } catch (err) {
+        if (err instanceof UniqueConstraintError) {
+
+            res.status(500).json({
+                error: 'Failed to register user: ${err}',
+            });
+        }
+    }
+});
+
+// login
+router.post('/login', async (req,res) => {
+    const {
+        email,
+        password
+    } = req.body;
+
+    try {
+        let loginUser = await UserModel.findOne ({
+            where: {
+                email
+            }
+        });
+    if (loginUser) {
+        let passwordComparison = await bcrypt.compare(password, loginUser.password);
+
+        if (passwordComparison) {
+            let token = jwt.sign({
+                id: loginUser.id
+            }, 
+            process.env.JWT_SECRET,
+            {
+                expiresIn: 60*60*24
+            }
+            );
+
+            res.status (200).json({
+                user: loginUser,
+                message: "User successfully logged in!",
+                token
+            });
+        }
+        }else{
+            res.status(401).json({
+                message: "Incorrect email or password"
+            });
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: 'Error loggin in!'
+        });
+    }
+})
 
 module.exports = router;
