b0929ffd15d89761e88b70062f03ba66d7c6e627
Derek <derek101225@gmail.com>
2021-03-12 20:12:39 -0500

    did post and put endpoints

--- server/controllers/reviewcontroller.js
+++ server/controllers/reviewcontroller.js
@@ -20,6 +20,47 @@ router.get('/put', async (req, res) => {
     }
 });
 
+router.post('/', validateSession, async (req, res) => {
+    const{
+        reviewTitle,
+        reviewersPost
+    } = req.body;
+
+    try {
+        const Review = await ReviewModel.create({
+            reviewTitle,
+            reviewersPost
+        });
+        res.status(200).json({
+            message: 'Review successfully created', Review,
+        });
+    }catch (err) {
+        res.status(500).json({
+            message: `Failed to create review ${err}`,
+
+        });
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    const {
+        reviewTitle,
+        reviewersPost
+    } = req.body;
+    try {
+        const reviewUpdated = ReviewModel.update({reviewTitle, reviewersPost},
+            {where: { id: req.params.id}});
+            res.status(200).json({
+                message: 'review successfully updated',
+            });
+
+    }catch(err) {
+        res.status(500).json({
+            message: `Failed to update review: ${err}`,
+        })
+    }
+});
+
 router.delete('/:id', async (req, res) => {
     try{
         await ReviewModel.destroy({
