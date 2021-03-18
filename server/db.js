e057209aa8c48ffb5ca5133e10dfa812a59dfe83
Tom <tomrambo95@yahoo.com>
2021-03-18 12:00:35 -0400

    updated .env

--- server/db.js
+++ server/db.js
@@ -1,9 +1,20 @@
 const Sequelize = require('sequelize');
 
+// const db = new Sequelize(process.env.DATABASE_URL, {
+//     dialect: "postgres",
+//     ssl: process.env.ENVIRONMENT === 'production'
+// }); 
+
 const db = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
-    ssl: process.env.ENVIRONMENT === 'production'
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnathorized: false
+        }
+    }
 }); 
+
 
 module.exports = db;
 
